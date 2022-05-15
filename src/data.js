828789a95e6502cad5601a23857dfc720d91c6fc
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-05-15 13:20:37 +0200

    create database with profileData

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const profileData = {
+    name: 'Michiel Roukens',
+    title: 'React Developer',
+    picture: '',
+    jobs: [{
+        type: 'job',
+        period: '2021/2022',
+        title: 'Freelance Developer',
+        company: 'Arejas',
+        location: 'The Hague, Netherlands',
+        description: 'Wordpress websites & webshops for different clients using Divi, Elmentor and WooCommerce'
+    }, {
+        type: 'job',
+        period: '2020',
+        title: 'Campervan builder',
+        company: 'Freelance',
+        location: 'Twisk, The Netherlands',
+        description: 'Converting cargovans to Campervans'
+    }, {
+        type: 'cert',
+        period: '2022',
+        company: 'codecademy',
+        location: 'At home',
+        title: 'FullStack Developer Career Path Course',
+        description: 'Become a coding champion'
+    }],
+    contactInfo: {
+        tel: '+31614819860',
+        mail: 'info@arejo.nl',
+        location: 'The Hague, Netherlands',
+        links: {
+            github: 'https://github.com/arejasverduras',
+            linkedin: 'https://www.linkedin.com/in/michielroukens/',
+            portfolio: ''
+        }
+    },
+    summary: 'Bad-ass coding Chamion',
+    keySkills: ['Building webapps from start to finish'],
+    techSkills: ['HTML', 'CSS', 'JavaScript','React','Redux','Node.js','Express.js','SQL','Git','GitHub','TDD','Mocha','JEST','React Testing Library'],
+    searchTerm: ''
+}
