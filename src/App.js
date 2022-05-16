0075aebf28411b454848832d9592ff86d14f32b3
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-05-16 12:32:47 +0200

    refactor to single function

--- src/App.js
+++ src/App.js
@@ -27,50 +27,42 @@ hobbies} = profileData;
       // Search Functionality
     const [searchTerm, setSearchTerm] = useState('')
 
-    const searchFilter = (data, cat) => {
+    const search = (data, cat,type) => {
       let result;
+        if (type === 'object'){
+          const contactArray = Object.entries(data);
 
       if (cat.includes(searchTerm.toLowerCase())){
-        result = data;
-      } else {
-        result =  data.filter(item =>  item.toLowerCase().includes(searchTerm.toLowerCase()))
-      }
-
+        result = contactArray;
+    } else {
+      result = contactArray.filter(([key, value]) => key.toLowerCase().includes(searchTerm.toLowerCase()));
+    }
+        } else if (type ==='array' ){
+          if (cat.includes(searchTerm.toLowerCase())){
+            result = data;
+          } else {
+            result =  data.filter(item =>  item.toLowerCase().includes(searchTerm.toLowerCase()))
+          }
+        } else if (type === 'Array of Objects'){
+            if (cat === 'jobs'){
+              result = jobs.filter(job => 
+                (job.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                job.type.toLowerCase().includes(searchTerm.toLowerCase()) 
+                  || job.title.toLowerCase().includes(searchTerm.toLowerCase()) 
+                  || job.company.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                  job.location.toLowerCase().includes(searchTerm.toLowerCase())));
+            } else if (cat ==='certifications'){
+              result = certifications.filter(cert => (
+                cert.type.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                cert.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                cert.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                cert.institute.toLowerCase().includes(searchTerm.toLowerCase() ||
+                cert.date.toLowerCase().includes(searchTerm.toLowerCase())
+              ))) 
+            }
+        }
       return result;
     }
-
-    const availableJobs = jobs.filter(job => 
-      (job.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      job.type.toLowerCase().includes(searchTerm.toLowerCase()) 
-        || job.title.toLowerCase().includes(searchTerm.toLowerCase()) 
-        || job.company.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        job.location.toLowerCase().includes(searchTerm.toLowerCase())));
-
-        const availableCertifications = certifications.filter(cert => (
-          cert.type.toLowerCase().includes(searchTerm.toLowerCase()) ||
-          cert.description.toLowerCase().includes(searchTerm.toLowerCase()) ||
-          cert.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-          cert.institute.toLowerCase().includes(searchTerm.toLowerCase() ||
-          cert.date.toLowerCase().includes(searchTerm.toLowerCase())
-        ))) 
-
-
-      // const contactFiltered = contactArray.filter(([key, value]) => key.toLowerCase().includes(searchTerm.toLowerCase()));
-
-      const contactSearch = (data, cat) => {
-        const contactArray = Object.entries(data);
-
-        let result;
-
-        if (cat.includes(searchTerm.toLowerCase())){
-          result = contactArray;
-      } else {
-        result = contactArray.filter(([key, value]) => key.toLowerCase().includes(searchTerm.toLowerCase()));
-      }
-
-      return result;
-    }
-
 
   return (
     <div className="App">
@@ -81,17 +73,19 @@ hobbies} = profileData;
         searchTerm={searchTerm}
         setSearchTerm={setSearchTerm} 
       />
-      <Contact contactInfo={contactSearch(contactInfo, 'contact')} />
+      <Contact contactInfo={search(contactInfo, 'contact','object')} />
       <Summary summary={summary} />
   <SideBar 
-    keySkills={searchFilter(keySkills, "key skills")}
-    techSkills={searchFilter(techSkills, "tech skills")}
-    mktSkills={searchFilter(mktSkills, "marketing skills")}
-    addSkills={searchFilter(addSkills, "additional skills")}
-    languages={searchFilter(languages, "languages")}
-    hobbies={searchFilter(hobbies, "hobbies")}
+    keySkills={search(keySkills, "key skills","array")}
+    techSkills={search(techSkills, "tech skills","array")}
+    mktSkills={search(mktSkills, "marketing skills","array")}
+    addSkills={search(addSkills, "additional skills","array")}
+    languages={search(languages, "languages","array")}
+    hobbies={search(hobbies, "hobbies", "array")}
      />
-    <Content jobs={availableJobs} certifications={availableCertifications} />
+    <Content 
+      jobs={search(jobs,'jobs','Array of Objects')} 
+      certifications={search(certifications,'certifications','Array of Objects')} />
     </div>
 
   );
