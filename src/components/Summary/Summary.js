f8f6182c43bb992c5e14d310a48b70ca6e4ded5e
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-05-18 14:16:23 +0200

    tweak objective / summary

--- src/components/Summary/Summary.js
+++ src/components/Summary/Summary.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { Widget } from '../Widget/Widget';
 
 export const Summary = ({summary, flipped}) => {
+    const objective = 
+    <>
+        <p><b>Startup founder turned developer.</b></p>
+        <p>Extremely passionate about building beautifull, practical and smart things. Self-starting, highly determined, obsessively persistent.</p>
+        <p>Having completed the Front-end section of the Full Stack Career Course on <a href="https://www.codecademy.com/learn/paths/full-stack-engineer-career-path">codecademy</a>, I am eager to practice my new found skills as a Front-end / React Developer. </p>
+        <p>Open for a junior position or Freelance assignment. Remote / hybrid preferred. <br/>Get in touch!</p>
+    </>
+
+//  ',
+
 
     return (
         <Widget 
             name="summary" 
             title="About me" 
-            content={summary}
+            content={objective}
             flipped={flipped}  />
     )
 }
