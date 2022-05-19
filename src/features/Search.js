6e9d7d61b7f1708970d00d400fa355533f60b1a2
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-05-19 11:16:09 +0200

    properly target the target.focus by adding first child

--- src/features/Search.js
+++ src/features/Search.js
@@ -3,16 +3,12 @@ import React from 'react'
 export const Search = ({searchTerm, setSearchTerm}) => {
     const handleChange = (e) => {
         setSearchTerm(e.target.value);
-        if (e.key === 'Enter'){
-            console.log('enter recognized')
-            e.target.blur();
-        }
     }
 
     const handleClick = (e) => {
         e.preventDefault();
         console.log('hallo??');
-        e.target.blur();
+        e.target.firstChild.blur();
     }
     
     return (
