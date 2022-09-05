3f79bc769368f26cf3488aa53a9d064c5a94d338
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-09-05 21:54:52 +0200

    spellchecking

--- src/data.js
+++ src/data.js
@@ -11,7 +11,7 @@ export const profileData = {
             title: 'Becoming a Full-Stack JavaScript Developer',
             company: 'Arejas',
             location: 'The Hague',
-            description: 'Bootcamp on Codecademy, Studying React / Express. Build and sold a campervan to be able to study fulltime. See: Education / About me'
+            description: 'Bootcamp on Codecademy, Studying React / Express. Build and sold a campervan to be able to study fulltime. See: Education / About me.'
         }, 
         {
         type: 'experience',
