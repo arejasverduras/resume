03bad49772c25a1309ea437de13d7106c8344b73
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-09-06 11:36:01 +0200

    import pdf instead of linking in component

--- src/components/DownloadPDF/DownloadPDF.js
+++ src/components/DownloadPDF/DownloadPDF.js
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import resume from './Michiel Roukens - React Developer - Full stack developer to be.pdf'
 
 export const DownloadPDF = ({pdf}) => {
-    if (!pdf) {
+    if (!resume) {
         return 
     } else {
     
     return (
-        <a className="pdfButton" href={pdf} download>
+        <a className="pdfButton" href={resume} download>
             Download resume as PDF
         </a>
     )
