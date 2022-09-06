ba842b7c198e38c9af211b9fdb0a4e69ebf94374
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-09-06 13:48:05 +0200

    properly resize to fit one page by selecting elements and changing css styles

--- src/components/ComponentToPrint/Printer.js
+++ src/components/ComponentToPrint/Printer.js
@@ -12,12 +12,51 @@ export const Printer = (props, ref) => {
 
   const centerOnPage = () => {
     let toStyle = document.getElementsByClassName('App')[0];
-    toStyle.style.fontSize="1.2rem"
+    toStyle.style.fontSize="0.6em";
+    toStyle.style.gridTemplateColumns= '30% 20% 55%';
+
+    let header = document.getElementsByClassName('header')[0];
+    header.style.height = '80px';
+    header.style.margin='0';
+
+    const allWidgets = Array.from(document.getElementsByClassName('Widget'));
+    for (let widget of allWidgets){
+      widget.style.padding="6px";
+    }
+
+    let profilePicture = document.getElementsByClassName('profilePicture')[0];
+    profilePicture.style.width = '80px';
+
+    let headerName = document.getElementsByClassName('headerName')[0];
+    headerName.style.fontSize='16px';
+
+    let headerTitle = document.getElementsByClassName('headerTitle')[0];
+    headerTitle.style.fontSize='12px';
+
   }
 
   const rePosition = () =>{
     let toReStyle = document.getElementsByClassName('App')[0];
     toReStyle.style.fontSize="1em"
+    toReStyle.style.gridTemplateColumns= '35% 33% 33%';
+
+    let header = document.getElementsByClassName('header')[0];
+    header.style.height = '';
+    header.style.marginBottom='30px';
+
+    const allWidgets = Array.from(document.getElementsByClassName('Widget'));
+    for (let widget of allWidgets){
+      widget.style.padding="";
+    }
+
+    let profilePicture = document.getElementsByClassName('profilePicture')[0];
+    profilePicture.style.width = '200px';
+
+    let headerName = document.getElementsByClassName('headerName')[0];
+    headerName.style.fontSize='26px';
+
+    let headerTitle = document.getElementsByClassName('headerTitle')[0];
+    headerTitle.style.fontSize='22px';
   }
 
   //download the file directly as PDF
@@ -28,12 +67,13 @@ export const Printer = (props, ref) => {
       <ReactToPrint
         onBeforeGetContent={centerOnPage}
         onAfterPrint={rePosition}
+        // pageStyle={pageStyle}
         trigger={() => <button className="printButton topButton">Print / Save as PDF</button>}
         content={() => componentRef.current}
       />
-      <DownloadPDF pdf={pdf}/>
+      {/* <DownloadPDF pdf={pdf}/> */}
   <App ref={componentRef} />
-  <DownloadPDF pdf={pdf}/>
+  {/* <DownloadPDF pdf={pdf}/> */}
   <ReactToPrint 
         onBeforeGetContent={centerOnPage}
         onAfterPrint={rePosition}
