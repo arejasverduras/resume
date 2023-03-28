3b772503091f6d992a454c0848f20d4037de0aaf
Arejas <62893479+arejasverduras@users.noreply.github.com>
2023-03-28 23:36:52 +0200

    update link to pdf

--- src/components/DownloadPDF/DownloadPDF.js
+++ src/components/DownloadPDF/DownloadPDF.js
@@ -3,6 +3,7 @@ import styles from './DownloadPDF.module.css';
 import resume from '../../documents/Michiel Roukens - React Developer - Front-end Developer.pdf'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons';
+import { profileData } from '../../data';
 
 export const DownloadPDF = () => {
     if (!resume) {
@@ -10,7 +11,7 @@ export const DownloadPDF = () => {
     } else {
     
     return (
-        <a className={styles.link} href={resume} target="_blank" rel='noreferrer'>
+        <a className={styles.link} href={profileData.pdf} target="_blank" rel='noreferrer'>
             Download as PDF<FontAwesomeIcon icon={faArrowDown}/> 
         </a>
     )
